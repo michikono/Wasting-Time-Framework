0ba28c0064ab81b913a2c79a76802383402f9dd0
Michi Kono <michikono@gmail.com>
2015-04-26 17:52:08 -0700

    Initial Commit

---
+++ Wasting Time Framework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wasting Time Framework
+//
+//  Created by Michi Kono on 4/26/15.
+//  Copyright (c) 2015 Michi Kono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
